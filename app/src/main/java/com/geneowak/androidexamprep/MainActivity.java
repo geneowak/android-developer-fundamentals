d5c918d8097a7822ac6ca1eede86714b6bc9ab1c
geneowak <teugeneowak@gmail.com>
2020-02-26 15:55:15 +0300

    - First interactive UI

--- app/src/main/java/com/geneowak/androidexamprep/MainActivity.java
+++ app/src/main/java/com/geneowak/androidexamprep/MainActivity.java
@@ -1,31 +1,47 @@
 package com.geneowak.androidexamprep;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.geneowak.androidexamprep.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // toasts //
-        // get app context
-        Context context = getApplicationContext();
-        // get text you'd like to display
-        String text = "Hello Toast";
-        // get the duration
-        int duration = Toast.LENGTH_SHORT;
+        mShowCount = findViewById(R.id.show_count);
+
+
+    }
+
+    // toasts //
+    public void showToast(View view) {
         // create the toast object
-        Toast toast = Toast.makeText(context, text, duration);
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        // positioning
+        // toast.setGravity(Gravity.TOP|Gravity.START, 0,0);
         // display the toast
         toast.show();
+        // test out logging an error
+        try {
+            int age = 1 / 0;
+            Log.d(LOG_TAG, "His age is " + age);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+    }
 
-
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
