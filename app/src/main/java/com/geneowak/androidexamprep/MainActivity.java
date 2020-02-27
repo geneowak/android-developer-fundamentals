030378bea4c4df5be5a0da4c51d22e5200663fa1
geneowak <teugeneowak@gmail.com>
2020-02-27 20:56:52 +0300

    Part B: The layout editor - homework

--- app/src/main/java/com/geneowak/androidexamprep/MainActivity.java
+++ app/src/main/java/com/geneowak/androidexamprep/MainActivity.java
@@ -1,8 +1,9 @@
 package com.geneowak.androidexamprep;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,13 +13,14 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private int mCount = 0;
     private TextView mShowCount;
+    private Button mZeroButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = findViewById(R.id.show_count);
-
+        mZeroButton = findViewById(R.id.button_zero);
 
     }
 
@@ -30,17 +32,27 @@ public class MainActivity extends AppCompatActivity {
         // toast.setGravity(Gravity.TOP|Gravity.START, 0,0);
         // display the toast
         toast.show();
-        // test out logging an error
-        try {
-            int age = 1 / 0;
-            Log.d(LOG_TAG, "His age is " + age);
-        } catch (Exception e) {
-            Log.e(LOG_TAG, e.getMessage());
-        }
     }
 
+    @SuppressLint("SetTextI18n")
     public void countUp(View view) {
         mCount++;
+        mZeroButton.setBackgroundColor(getColor(R.color.colorAccent));
+        // update the color of the button depending on whether the new count is odd or even
+        if (mCount % 2 == 0) {
+            view.setBackgroundColor(getColor(android.R.color.holo_purple));
+        } else {
+            view.setBackgroundColor(getColor(android.R.color.holo_green_light));
+        }
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void resetCount(View view) {
+        mCount = 0;
+        // set the color to zero
+        mZeroButton.setBackgroundColor(getColor(android.R.color.darker_gray));
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
     }
